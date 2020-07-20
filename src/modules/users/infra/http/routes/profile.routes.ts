3571c362e34b64f757853d6c9d383b23482bebd0
Filipez <filipezmarron@hotmail.com>
2020-07-20 16:07:16 -0300

    ✨ Add validation to post/put routes

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProfileController from '../controllers/ProfileController';
@@ -10,7 +10,19 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/', profileController.update);
 profileRouter.get('/', profileController.show);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string(),
+      old_password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
