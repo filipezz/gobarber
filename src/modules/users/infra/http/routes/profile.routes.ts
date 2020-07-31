1527376d7207fb56575792b0112bfa1e6375a82b
Filipez <filipezmarron@hotmail.com>
2020-07-31 19:38:25 -0300

    Fix url from reset password and remove required fields in update profile

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -15,8 +15,8 @@ profileRouter.put(
   '/',
   celebrate({
     [Segments.BODY]: {
-      name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      name: Joi.string(),
+      email: Joi.string().email(),
       password: Joi.string(),
       old_password: Joi.string(),
       password_confirmation: Joi.string().valid(Joi.ref('password')),
